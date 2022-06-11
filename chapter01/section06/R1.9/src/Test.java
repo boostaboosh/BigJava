f7a17bf0e72bd3518b11c8d2305df1bf7f942b1b
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-06-11 09:48:22 +0100

    Answer review questions: 1.7, 1.8, 1.9
    This commit implements the answers to questions 1.7, 1.8, 1.9 on page 1. They regard sections 5 called Analzing Your First Program and 6 called Errors

---
+++ chapter01/section06/R1.9/src/Test.java
@@ -0,0 +1,13 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello", "World!"); // no suitable method found for println(java.lang.String,java.lang.String)
+
+//        A compile-time error is an error that is found by the compiler when something is wrong with the program according to the rules of the language.
+//        For this reason compile-time errors are also called syntax errors: because they are linguistic errors in the programming language.
+
+//        When the compiler finds one or more errors, it refuses to translate the program source files into Java virtual machine instructions,
+//        and as a consequence you have no program that you can run.
+    }
+}
