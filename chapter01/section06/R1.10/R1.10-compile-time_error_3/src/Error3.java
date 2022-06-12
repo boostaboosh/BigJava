659814fc5a05263851d1561b09f069486ce90e69
boostaboosh <mail.laurencesnow@gmail.com>
2022-06-12 11:04:42 +0100

    Answers review question R1.10 page 1

---
+++ chapter01/section06/R1.10/R1.10-compile-time_error_3/src/Error3.java
@@ -0,0 +1,9 @@
+public class Error3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Classes contain methods. Methods contain a collection of instructions that describe how to carry out a particular task." +
+                "Instructions in Java are also called statements. In Java every statement must end in a semicolon." +
+                "The compiler uses the semicolon to find where one statement ends and the next one starts.") // java: ';' expected
+    }
+}
