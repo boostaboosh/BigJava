8dd5d7df8b66074965e71794b5bb0a18913d8c40
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-06-12 11:04:42 +0100

    Answers review question R1.10 page 1

---
+++ chapter01/section06/R1.10/R1.10-compile-time_error_2/src/Error2.java
@@ -0,0 +1,11 @@
+public class Error2
+{
+    public static void maiN (String[] args)
+    {
+        System.out.println("Java methods are case sensitive.");
+        // compile-time errors are found by the compiler at compile time.
+        // They are errors according the rules of the programming language that the compiler translates.
+        // For this reason compile time errors are often called syntax errors.
+        // Compile-time errors stop your program from being translated to machine code and executed.
+    }
+}
