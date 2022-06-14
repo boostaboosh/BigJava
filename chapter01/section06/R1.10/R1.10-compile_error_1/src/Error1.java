82684c7405667a8782e51c5f855a7c2af143de20
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-06-14 16:20:40 +0100

    Add compile time error definition to error 1 QR1.10

--- chapter01/section06/R1.10/R1.10-compile_error_1/src/Error1.java
+++ chapter01/section06/R1.10/R1.10-compile_error_1/src/Error1.java
@@ -2,6 +2,10 @@ public class Error1
 {
     public static void main(String[] args)
     {
+        // compile-time errors are found by the compiler at compile time.
+        // They are errors according the rules of the programming language that the compiler translates.
+        // For this reason compile time errors are often called syntax errors.
+        // Compile-time errors stop your program from being translated to machine code and executed.
         System.println("Hello, nobody!"); // java: cannot find symbol
     }
 }
