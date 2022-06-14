70815296d08061c059ec84dce2403294d12a362d
boostaboosh <mail.laurencesnow@gmail.com>
2022-06-14 16:17:00 +0100

    Correct R1.10 error 2
    A missing main method is a run-time error, and not a compile-time error.

--- chapter01/section06/R1.10/R1.10-compile-time_error_2/src/Error2.java
+++ chapter01/section06/R1.10/R1.10-compile-time_error_2/src/Error2.java
@@ -2,7 +2,11 @@ public class Error2
 {
     public static void maiN (String[] args)
     {
-        System.out.println("Java methods are case sensitive.");
+        // This is actually a run time error because the compiler does not consider this to be the same as the main method because
+        // the Java language is case-sensitive: Upper and lowercase letters are considered to be completely different from each other.
+        // The compiler will cheerfully compile the maiN method.
+        // But when the Java virtual machine reads the compiled file, it will complain about the missing main method and refuse to run the program.
+        System.out.println("The Java language is case sensitive.");
         // compile-time errors are found by the compiler at compile time.
         // They are errors according the rules of the programming language that the compiler translates.
         // For this reason compile time errors are often called syntax errors.
