11dd525c57991c1193d0b254f17486f4290fcf4e
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-06-22 14:41:09 +0100

    Add chapter 2 review exercise 2.11

---
+++ chapter02/section03/R2.11/src/HelloToUpperCase.java
@@ -0,0 +1,9 @@
+public class HelloToUpperCase
+{
+    public static void main(String[] args)
+    {
+        String message = "Hello";
+        message = message.toUpperCase();
+        System.out.print(message);
+    }
+}
