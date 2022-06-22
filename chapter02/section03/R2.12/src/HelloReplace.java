2739c64f45e36a4d34d64d9d876688805947a40b
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-06-22 14:46:05 +0100

    Add chapter 2 review exercise 12

---
+++ chapter02/section03/R2.12/src/HelloReplace.java
@@ -0,0 +1,9 @@
+public class HelloReplace
+{
+    public static void main (String[] args)
+    {
+        String message = "Hello";
+        message = message.replace("H", "h");
+        System.out.print(message);
+    }
+}
