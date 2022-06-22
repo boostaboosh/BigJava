90be479837b8bd1af066dfaea741b53dfe02227d
boostaboosh <mail.laurencesnow@gmail.com>
2022-06-22 13:46:00 +0100

    Add chapter02 review exercises 6-7-8

---
+++ chapter02/section02/R2.8/src/whatIsWrongWithMystery.java
@@ -0,0 +1,9 @@
+public class whatIsWrongWithMystery
+{
+    public static void main(String[] args)
+    {
+        int mystery = 1;
+        mystery = 1 - 2 * mystery;
+        int mystery = mystery + 1;
+    }
+}
