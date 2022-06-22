1cb0563dec337a5406073ece378821f3801afb61
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-06-22 13:46:00 +0100

    Add chapter02 review exercises 6-7-8

---
+++ chapter02/section02/R2.7/src/whatIsTheValueOfMystery.java
@@ -0,0 +1,11 @@
+public class whatIsTheValueOfMystery
+{
+    public static void main(String[] args)
+    {
+        int mystery = 1;
+        mystery = 1 - 2 * mystery;
+        mystery = mystery + 1;
+
+        System.out.print(mystery);
+    }
+}
