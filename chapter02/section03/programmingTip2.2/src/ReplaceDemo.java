65a11e845fb525559c8c8692df7993a0f3ac31e0
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-06-22 09:26:07 +0100

    Add programming tip 2.2

---
+++ chapter02/section03/programmingTip2.2/src/ReplaceDemo.java
@@ -0,0 +1,9 @@
+public class ReplaceDemo
+{
+    public static void main(String[] args)
+    {
+        String river = "Mississippi";
+        System.out.println(river.replace("issipp", "our"));
+        System.out.println(river.replace("i", "x"));
+    }
+}
