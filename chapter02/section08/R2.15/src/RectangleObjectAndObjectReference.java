a12d8b47b74a87647af9a0731e065063f95593d0
boostaboosh <mail.laurencesnow@gmail.com>
2022-06-24 16:11:58 +0100

    Add review exercise 2.15
    R2.15 Give the Java code for constructing an object of class Rectangle , and for declaring an
    object variable of class Rectangle .

---
+++ chapter02/section08/R2.15/src/RectangleObjectAndObjectReference.java
@@ -0,0 +1,18 @@
+import java.awt.Rectangle;
+
+public class RectangleObjectAndObjectReference
+{
+    public static void main(String[] args) {
+        /**
+         * R2.15 Give the Java code for constructing an object of class Rectangle , and for declaring an
+         * object variable of class Rectangle .
+         */
+
+        // Java code for constructing an object of class Rectangle
+        new Rectangle(0, 0, 100, 200);
+
+        // Java code for declaring an object variable of class Rectangle
+        Rectangle objectVariable;
+        // In Java an object variable stores a reference to the location of an object, and not the object itself.
+    }
+}
