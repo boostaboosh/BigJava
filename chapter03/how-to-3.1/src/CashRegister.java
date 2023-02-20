26ce93a6eb0a932d7c16bbbb3b1d253d106dad73
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2023-02-20 10:46:33 -0500

    Add how-to-3.1 exercise from chapter 3

---
+++ chapter03/how-to-3.1/src/CashRegister.java
@@ -0,0 +1,44 @@
+/**
+ * A cash register totals up sales and computer change due.
+ */
+public class CashRegister {
+	private double purchase;
+	private double payment;
+	
+	/**
+	 * Constructs a cash register with no money in it.
+	 */
+	public CashRegister() 
+	{
+	}
+	
+	/**
+	 * Records the sale of an item.
+	 * @param amount the price of the item
+	 */
+	public void recordPurchase(double amount) 
+	{
+		purchase = purchase + amount;
+	}
+	
+	/**
+	 * Processes a payment received from the customer.
+	 * @param amount the amount of the payment
+	 */
+	public void receivePayment(double amount)
+	{
+		payment = payment + amount;
+	}
+	
+	/**
+	 * Computes the change due and resets the machine for the next customer.
+	 * @return the change due to the customer
+	 */
+	public double giveChange()
+	{
+		double change = payment - purchase;
+		purchase = 0;
+		payment = 0;
+		return change;
+	}
+}
