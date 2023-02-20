9475370992a1bdb51ca017fa6ad5ffdc18a00c65
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2023-02-20 17:34:46 -0500

    Rephrase sentence in review exercise 2 chapter 3

---
+++ chapter03/how-to-3.1/src/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"CashRegister","l":"CashRegister()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"CashRegister","l":"giveChange()"},{"p":"<Unnamed>","c":"CashRegister","l":"receivePayment(double)"},{"p":"<Unnamed>","c":"CashRegister","l":"recordPurchase(double)"}];updateSearchResults();
