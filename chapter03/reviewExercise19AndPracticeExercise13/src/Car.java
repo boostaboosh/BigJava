1f36c4aaf1ebae93e07f38eca682db23cbb1453b
lorenzo-2022 <mail.laurencesnow@gmail.com>
2023-02-24 15:59:16 -0500

    Complete chap03 practice exercise 13

--- chapter03/reviewExercise19AndPracticeExercise13/src/Car.java
+++ chapter03/reviewExercise19AndPracticeExercise13/src/Car.java
@@ -1,4 +1,47 @@
-
-public class Car {
-
+/**
+ * A car has a certain fuel efficiency and can drive 
+ * using up fuel in its tank.
+ */
+public class Car 
+{
+	private double fuelEfficiencyInLitresPerKm;
+	private double gasInTank;
+	
+	/**
+	 * Construct a car with the specified fuel efficiency.
+	 * @param fuelEfficiencyInLitresPerKm
+	 */
+	public Car(double fuelEfficiencyInLitresPerKm)
+	{
+		this.fuelEfficiencyInLitresPerKm = fuelEfficiencyInLitresPerKm;
+		this.gasInTank = 0;
+	}
+	
+	/**
+	 * Drives a car for the specified distance using up fuel in the tank.
+	 * @param distance the distance of the drive
+	 */
+	public void drive(double distance)
+	{	
+		double litersOfGasolineUsed = distance * fuelEfficiencyInLitresPerKm;
+		gasInTank = gasInTank - litersOfGasolineUsed;
+	}
+	
+	/**
+	 * Returns the current amount of gasoline in the fuel tank.
+	 * @return the amount of gasoline in the fuel tank
+	 */
+	public double getGasInTank()
+	{
+		return gasInTank;
+	}
+	
+	/**
+	 * Adds gasoline to the fuel tank.
+	 * @param litersOfGas the number of litres of gasoline to add to the fuel tank
+	 */
+	public void addGas(double litresOfGas)
+	{
+		gasInTank = gasInTank + litresOfGas;
+	}
 }
--- chapter03/reviewExercise19AndPracticeExercise13/src/CarTester.java
+++ chapter03/reviewExercise19AndPracticeExercise13/src/CarTester.java
@@ -5,13 +5,13 @@ public class CarTester
 {
 	public static void main(String[] args)
 	{
-		Car ferrariEnzo = new Car(3.4);
+		Car ferrariEnzo = new Car(0.34);
 		ferrariEnzo.addGas(50);
 		ferrariEnzo.drive(100);
 		ferrariEnzo.addGas(55);
 		ferrariEnzo.drive(25);
 		double gasLeft = ferrariEnzo.getGasInTank();
 		System.out.println(gasLeft);
-		System.out.println("Expected: 68.2352941176");
+		System.out.println("Expected: 62.5");
 	}
 }
