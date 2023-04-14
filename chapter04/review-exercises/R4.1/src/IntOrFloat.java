58f0fbff5a9ca4f54a81f9ad453d3c088645380d
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2023-04-14 19:09:05 -0400

    Add chap04 review exercise 01

---
+++ chapter04/review-exercises/R4.1/src/IntOrFloat.java
@@ -0,0 +1,10 @@
+/**
+ * A class for review exercise 4.1
+ */
+public class IntOrFloat
+{
+    static final int DAYS_IN_WEEK = 7;
+    int remainingSemesterDays;
+    static final double CENTIMETRES_IN_AN_INCH = 2.54;
+    double tallestClassmateHeightInCm = 197.0;
+}
