8878f177570695a0ffdb5547599dda80c258b346
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2023-04-14 19:12:57 -0400

    Add chap04 review exercise 02

---
+++ chapter04/review-exercises/R4.2/src/Mystery.java
@@ -0,0 +1,10 @@
+public class Mystery
+{
+    public static void main(String[] args)
+    {
+        int mystery = 1;
+        mystery = 1 - 2 * mystery;
+        mystery = mystery + 1;
+        System.out.println(mystery + "\nExpected: 0");
+    }
+}
