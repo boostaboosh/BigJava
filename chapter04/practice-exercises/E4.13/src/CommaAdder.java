01338d367555ab66ba81e27abf88aa27744592b8
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2023-04-24 15:34:18 -0400

    Add chap04 unstaged exercise files

--- chapter04/practice-exercises/E4.13/src/CommaAdder.java
+++ chapter04/practice-exercises/E4.13/src/CommaAdder.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CommaAdder {
+import java.util.Scanner;
+
+/**
+ * A class which adds a comma to separate the thousands of an integer input between 1,000 and 999,999.
+ */
+public class CommaAdder
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        // output message telling user to input an integer between 1000 and 999999
+        System.out.println("Input an integer between 1000 and 999999 to which you want to add a comma: ");
+        // input = user keyboard input
+        String number = input.next();
+        // rightOfThousands = string from third last character to last character of input number
+        String rightOfThousands = number.substring(number.length() - 3, number.length());
+        // LeftOfHundreds = string from first character to fourth last character of input number
+        String leftOfHundreds = number.substring(0, number.length() - 3);
+        // output leftOfHundreds + , + rightOfThousands
+        System.out.println(leftOfHundreds + "," + rightOfThousands);
+    }
 }
--- chapter04/practice-exercises/E4.3/src/Arithmetic.java
+++ chapter04/practice-exercises/E4.3/src/Arithmetic.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Arithmetic {
+import java.util.Scanner;
+
+/**
+ * A class which for chap04 practice exercise 4.3.
+ */
+public class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter two integers: ");
+        int firstNumber = input.nextInt();
+        int secondNumber = input.nextInt();
+
+        // sum
+        int sum = firstNumber + secondNumber;
+        System.out.printf("\nsum: %d", sum);
+
+        // difference
+        int difference = firstNumber - secondNumber;
+        System.out.printf("\ndifference: %d", difference);
+
+        // product
+        int product = firstNumber * secondNumber;
+        System.out.printf("\nproduct: %d", product);
+
+        // average
+        double doubleSum = sum;
+        double average = doubleSum / 2;
+        System.out.printf("\naverage: %10.2f", average);
+
+        // distance (absolute value of the difference)
+        int distance = Math.abs(difference);
+        System.out.printf("\ndistance: %d", distance);
+
+        // maximum (the larger of the two)
+        int maximum = Math.max(firstNumber, secondNumber);
+        System.out.printf("\nmaximum: %d", maximum);
+
+        // minimum (the smaller of the two)
+        int minimum = Math.min(firstNumber, secondNumber);
+        System.out.printf("\nminimum: %d", minimum);
+    }
 }
