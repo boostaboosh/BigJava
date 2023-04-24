73e9806614d6f917fab792c28dff330a96cbc3c9
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2023-04-24 15:34:18 -0400

    Add chap04 unstaged exercise files

--- chapter04/practice-exercises/E4.12/src/CommaRemover.java
+++ chapter04/practice-exercises/E4.12/src/CommaRemover.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CommaRemover {
+import java.util.Scanner;
+
+/**
+ * A class for solving chap 4 practice exercise 12 Big Java.
+ */
+public class CommaRemover
+{
+    public static void main(String[] args)
+    {
+//        output message to user saying to input number with comma between 1,000 and 999,999
+        System.out.println("Enter number between 1,000 and 999,999 with the comma: ");
+//        numberWithComma = keyboard input
+        Scanner input = new Scanner(System.in);
+        String numberWithComma = input.next();
+//        position of last letter = numberWithComma.length - 1
+        int lastLetterPosition = numberWithComma.length() - 1;
+//        position of comma = position of last letter - 3
+        int positionOfComma = lastLetterPosition - 3;
+//        first half of number = string from position 0 to before comma
+        String firstHalfOfNumber = numberWithComma.substring(0, positionOfComma);
+//        second half of number = string from after comma to end of string
+        String secondHalfOfNumber = numberWithComma.substring(positionOfComma + 1, numberWithComma.length());
+//        output first half of number concatenated with second half of number
+        System.out.println(firstHalfOfNumber + secondHalfOfNumber);
+    }
 }
--- chapter04/practice-exercises/E4.9/src/MilesPerGallon.java
+++ chapter04/practice-exercises/E4.9/src/MilesPerGallon.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class MilesPerGallon {
+import java.util.Scanner;
+
+public class MilesPerGallon
+{
+    public static void main(String[] args)
+    {
+        // input = keyboardInput
+        Scanner input = new Scanner(System.in);
+
+        // output "Enter gallons of gas currently in tank: "
+        System.out.println("Enter gallons of gas currently in the car's gas tank: ");
+        // gallonsInTank = nextDoubleInInput
+        double gallonsInTank = input.nextDouble();
+
+        // output "Enter car fuel efficiency in MPG: "
+        System.out.println("Enter car fuel efficiency in miles per gallon (MPG): ");
+        // fuelEfficiencyMPG = nextDoubleInput
+        double fuelEfficiencyMPG = input.nextDouble();
+
+        // output "Enter price of fuel per gallon: "
+        System.out.println("Enter price of fuel per gallon: ");
+        // pricePerGallon = nextDoubleInput
+        double pricePerGallon = input.nextDouble();
+
+        // gallonsPerHundredMiles = 100 / fuelEfficiencyMPG
+        double gallonsPerHundredMiles = 100 / fuelEfficiencyMPG;
+        // costPerHundredMiles = gallonsPerHundredMiles * pricePerGallon
+        double costPerHundredMiles = gallonsPerHundredMiles * pricePerGallon;
+        // distanceRemaining = gallonsInTank * fuelEfficiencyMPG
+        double distanceRemaining = gallonsInTank * fuelEfficiencyMPG;
+
+        // output costPerHundredMiles
+        System.out.printf("The cost of driving the car 100 miles is %.2f CAD. ", costPerHundredMiles);
+        // output distanceRemaining
+        System.out.printf("\nThe car can travel a remaining %.2f miles before running out of gas. ", distanceRemaining);
+    }
 }
