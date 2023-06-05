f0d726ecd6cabdba90b41d1bb99a3cb0529ae0ac
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2023-06-05 16:39:40 +0200

    Add ch4 practice ex16

---
+++ chapter05/practice-exercises/E5.16/src/CardTester.java
@@ -0,0 +1,22 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A class which tests the Card class.
+ */
+public class CardTester
+{
+   public static void main(String[] args)
+   {
+      Logger.getGlobal().setLevel(Level.INFO);
+
+      Card queenOfSpades = new Card("QS");
+      Logger.getGlobal().info(queenOfSpades.getDescription() + "\nExpected: Queen of Spades");
+
+      Card tenOfHearts = new Card("10H");
+      Logger.getGlobal().info(tenOfHearts.getDescription() + "\nExpected: Ten of Hearts");
+
+      Card threeOfClubs = new Card("3C");
+      Logger.getGlobal().info(threeOfClubs.getDescription() + "\nExpected: Three of Clubs");
+   }
+}
