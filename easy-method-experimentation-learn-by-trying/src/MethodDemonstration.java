a6c990d5635d55467bc5c11fc2dc5ccaf16cd5b1
boostaboosh <mail.laurencesnow@gmail.com>
2023-07-27 07:57:56 +0100

    Add a program for experimenting with methods

---
+++ easy-method-experimentation-learn-by-trying/src/MethodDemonstration.java
@@ -0,0 +1,16 @@
+/**
+ * A class for experimenting with methods I learn about so that I can learn about it.
+ * Then I can see with my own eyes what a method does and run experiments with it.
+ * I will keep this program open in the background as I learn so that I can make this kind of
+ * experimentation easy and natural.
+ */
+public class MethodDemonstration
+{
+    /**
+     * The main method where the Java program begins execution.
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+    }
+}
