0c33e78d29e6de454ab9d4bfa3bb198cb179546a
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-01-06 14:59:06 +0100

    Add ch06 prog proj05 Game of Nim

--- chapter06/programming-projects/P6.5/src/NimPlayer.java
+++ chapter06/programming-projects/P6.5/src/NimPlayer.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * NimPlayer class runs a program which asks users if they want to play
@@ -13,16 +16,45 @@ public class NimPlayer
     */
    public static void main(String[] args)
    {
+      Logger.getGlobal().setLevel(Level.OFF);
+
       Scanner scanner = new Scanner(System.in);
       System.out.print("Do you want to play a game of Nim (y/n)? ");
       String userAnswer = scanner.next();
       while (userAnswer.equals("y"))
       {
-         GameOfNim nimGame = new GameOfNim();
+         Random randomNumberGenerator = new Random();
+
+         GameOfNim.TurnOrder turnOrder = GameOfNim.TurnOrder.COMPUTER_FIRST;
+         int randomOneOrTwo = randomNumberGenerator.nextInt(2);
+         if (randomOneOrTwo == 1)
+         {
+            turnOrder = GameOfNim.TurnOrder.USER_FIRST;
+         }
+         System.out.println("\nTurn order is " + turnOrder);
+
+         GameOfNim nimGame = new GameOfNim(turnOrder);
          while (!nimGame.isOver())
          {
-            nimGame.nextTurn();
+            if (turnOrder == GameOfNim.TurnOrder.USER_FIRST)
+            {
+               nimGame.nextPlayerTurn();
+               if (!nimGame.isOver())
+               {
+                  nimGame.nextComputerTurn();
+               }
+            }
+            else
+            {
+               nimGame.nextComputerTurn();
+               if (!nimGame.isOver())
+               {
+                  nimGame.nextPlayerTurn();
+               }
+            }
          }
+         System.out.print("Do you want to play another game of Nim (y/n)? ");
+         userAnswer = scanner.next();
       }
       System.out.print("Thanks for playing. Good bye!");
    }
