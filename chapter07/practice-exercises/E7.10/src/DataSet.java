15b09b37b4318648be2b25c6c2245e5869b10197
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-01-31 12:10:21 +0000

    Add ch07 prac ex10 pub interface & tester

---
+++ chapter07/practice-exercises/E7.10/src/DataSet.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+/**
+ * Dataset objects store a number of values of type double.
+ */
+public class DataSet
+{
+
+   /**
+    * Constructs a new DataSet object with no values.
+    */
+   public DataSet(int maximumNumberOfValues)
+   {
+      // TODO: implementation filled in later
+   }
+
+   /**
+    * Adds a value to the data set.
+    * @param valueToAdd the value to add to the data set
+    */
+   public void add(int valueToAdd)
+   {
+      // TODO: implementation filled in later
+   }
+
+   /**
+    * Computes the sum of all elements in the data set.
+    * @return the sum of all elements in the data set
+    */
+   public int getSum()
+   {
+      // TODO: implementation filled in later
+      return 0;
+   }
+
+   /**
+    * Computer the average of all elements in the data set.
+    * @return the average of all elements in the data set
+    */
+   public int getAverage()
+   {
+      // TODO: implementation filled in later
+      return 0;
+   }
+
+   /**
+    * Gets the highest value in the data set.
+    * @return the highest value in the data set.
+    */
+   public int getMaximum()
+   {
+      // TODO: implementation filled in later
+      return 0;
+   }
+
+   /**
+    * Gets the lowest value in the data set.
+    * @return the lowest value in the data set
+    */
+   public int getMinimum()
+   {
+      // TODO: implementation filled in later
+      return 0;
+   }
+}
---
+++ chapter07/practice-exercises/E7.10/src/DataSetTester.java
@@ -0,0 +1,25 @@
+/**
+ * A DatsSetTester tests the methods of the DataSet class
+ * by using a DataSet object and outputting its expected
+ * behaviour and its actual behaviour. This way we can
+ * see if objects behave as expected.
+ */
+public class DataSetTester
+{
+   public static void main(String[] args)
+   {
+      DataSet dataset = new DataSet(3);
+      dataset.add(3.5);
+      dataset.add(2);
+      dataset.add(-10.501);
+      dataset.add(1);
+      double sum = dataset.getSum();
+      System.out.println("Expected sum is -5.01, actual is " + sum);
+      double average = dataset.getAverage();
+      System.out.println("Expected average is -1.669, actual is " + average);
+      double maximum = dataset.getMaximum();
+      System.out.println("Expected max is 3.5, actual is " + maximum);
+      double minimum = dataset.getMinimum();
+      System.out.println("Expected min is -10.510, actual is " + minimum);
+   }
+}
