fe18d938b2484ab49d5fcf29d887260e0a986efe
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-02-26 10:09:53 +0000

    Add ch09 proj09 3InAColumn method & test

--- chapter07/programming-projects/P7.9/TestSuite/run-tests.bat
+++ chapter07/programming-projects/P7.9/TestSuite/run-tests.bat
@@ -1,1 +1,2 @@
 java -cp "../out/production/P7.9/" TicTacToeGamePlayer < threeInARowTestInputs.txt > threeInARowTestOutput.txt
+java -cp "../out/production/P7.9/" TicTacToeGamePlayer < threeInAColumnTestInputs.txt > threeInAColumnTestOutput.txt
