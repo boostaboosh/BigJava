7e4ad8c88273166390285df1e43ce565fba00d63
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-02-29 16:53:41 +0000

    Add methods-asked-to-supply start of P7.10 video poker

---
+++ chapter07/programming-projects/P7.10-VideoPokerSimulator/src/VideoPokerSimulator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * This class contains a program which simulates a game of video poker.
+ */
+public class VideoPokerSimulator
+{
+   /**
+    * Starts the program.
+    * @param args the program starting arguments
+    */
+   public static void Main(String[] args)
+   {
+      VideoPokerGame game = new VideoPokerGame();
+      while (game.askForToken())
+      {
+         game.shuffleDeck();
+         game.presentTopFiveCards();
+         game.playHand();
+      }
+   }
+}
