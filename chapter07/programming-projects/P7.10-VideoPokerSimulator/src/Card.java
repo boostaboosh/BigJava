252f486c9db05b613cf38ab3c34bd8020d9938f0
boostaboosh <mail.laurencesnow@gmail.com>
2024-03-02 11:04:59 +0000

    Add askForToken & Deck creation implementation to ch07 proj10

--- chapter07/programming-projects/P7.10-VideoPokerSimulator/src/Card.java
+++ chapter07/programming-projects/P7.10-VideoPokerSimulator/src/Card.java
@@ -15,12 +15,28 @@ public class Card
    /**
     * Constructs a new card object.
     */
-   public Card(Suit suit, int value)
+   public Card(int value, Suit suit)
    {
-      // TODO: fill implementation
-      this.suit = suit;
       this.value = value;
+      this.suit = suit;
    }
 
    // methods
+   /**
+    * Get the value of this card as a number from 1 to 13.
+    * @return the value of this card as a number from 1 to 13
+    */
+   public int getValue()
+   {
+      return this.value;
+   }
+
+   /**
+    * Get the suit of this card.
+    * @return the suit of this card
+    */
+   public Card.Suit getSuit()
+   {
+      return this.suit;
+   }
 }
