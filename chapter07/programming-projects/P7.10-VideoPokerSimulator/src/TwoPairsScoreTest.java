a81540e79cd78acc7d139aa4d40de9078ff7206a
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-03-09 16:49:01 +0000

    Add method & test for hand with two pairs

---
+++ chapter07/programming-projects/P7.10-VideoPokerSimulator/src/TwoPairsScoreTest.java
@@ -0,0 +1,22 @@
+public class TwoPairsScoreTest
+{
+   public static void main(String[] args)
+   {
+      Card one = new Card(3, Card.Suit.HEARTS);
+      Card two = new Card(3, Card.Suit.CLUBS);
+      Card three = new Card(1, Card.Suit.CLUBS);
+      Card four = new Card(12, Card.Suit.SPADES);
+      Card five = new Card(12, Card.Suit.SPADES);
+      Card[] twoPairHand = { one, two, three, four, five };
+      Card six = new Card(5, Card.Suit.DIAMONDS);
+      Card[] notTwoPairHand = { one, two, three, four, six };
+
+      VideoPokerGame game = new VideoPokerGame();
+
+      System.out.print("hasTwoPairs Expected true: ");
+      System.out.println(game.hasTwoPairs(twoPairHand));
+
+      System.out.print("hasTwoPairs Expected false: ");
+      System.out.println(game.hasTwoPairs(notTwoPairHand));
+   }
+}
---
+++ chapter07/programming-projects/P7.10-VideoPokerSimulator/tests/run-tests.bat
@@ -0,0 +1,6 @@
+# Compile all Java files in the src folder. The compiled class files will be placed in the ../out/production/P7.10-VideoPokerSimulator/ directory.
+javac -d "../out/production/P7.10-VideoPokerSimulator/" src/*.java
+
+# After compilation, run the TwoPairsScoreTest class. The classpath is set to the directory containing the compiled class files.
+# The output of the test is redirected to a file named twoPairsScoreTestOutput.txt.
+java -cp "../out/production/P7.10-VideoPokerSimulator/" TwoPairsScoreTest > tests/twoPairsScoreTestOutput.txt
