7b50ebcce30c3cb8f7ca2b52fea553f472602370
boostaboosh <mail.laurencesnow@gmail.com>
2024-03-11 10:04:53 +0000

    Fix duplicate hand cards in two pairs score test

--- chapter07/programming-projects/P7.10-VideoPokerSimulator/src/TwoPairsScoreTest.java
+++ chapter07/programming-projects/P7.10-VideoPokerSimulator/src/TwoPairsScoreTest.java
@@ -6,8 +6,9 @@ public class TwoPairsScoreTest
       Card two = new Card(3, Card.Suit.CLUBS);
       Card three = new Card(1, Card.Suit.CLUBS);
       Card four = new Card(12, Card.Suit.SPADES);
-      Card five = new Card(12, Card.Suit.SPADES);
+      Card five = new Card(12, Card.Suit.DIAMONDS);
       Card[] twoPairHand = { one, two, three, four, five };
+
       Card six = new Card(5, Card.Suit.DIAMONDS);
       Card[] notTwoPairHand = { one, two, three, four, six };
 
