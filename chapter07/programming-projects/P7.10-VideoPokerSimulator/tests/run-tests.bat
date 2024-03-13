461d098567e60e8597ca68bdd50ec1ab2b959bbe
boostaboosh <mail.laurencesnow@gmail.com>
2024-03-13 12:36:22 +0000

    Fix run-tests batch command so that output is in project dir

--- chapter07/programming-projects/P7.10-VideoPokerSimulator/tests/run-tests.bat
+++ chapter07/programming-projects/P7.10-VideoPokerSimulator/tests/run-tests.bat
@@ -1,16 +1,16 @@
 # Compile all Java files in the src folder. The compiled class files will be placed in the ../out/production/P7.10-VideoPokerSimulator/ directory.
-javac -d "../out/production/P7.10-VideoPokerSimulator/" src/*.java
+javac -d "../out/production/P7.10-VideoPokerSimulator/" ../src/*.java
 
 # After compilation, run the TwoPairsScoreTest class. The classpath is set to the directory containing the compiled class files.
 # The output of the test is redirected to a file named twoPairsScoreTestOutput.txt.
-java -cp "../out/production/P7.10-VideoPokerSimulator/" TwoPairsScoreTest > tests/twoPairsScoreTestOutput.txt
+java -cp "../out/production/P7.10-VideoPokerSimulator/" TwoPairsScoreTest > twoPairsScoreTestOutput.txt
 
-java -cp "../out/production/P7.10-VideoPokerSimulator/" FullHouseScoreTest > tests/fullHouseScoreTestOutput.txt
+java -cp "../out/production/P7.10-VideoPokerSimulator/" FullHouseScoreTest > fullHouseScoreTestOutput.txt
 
-java -cp "../out/production/P7.10-VideoPokerSimulator/" StraightScoreTest > tests/straightScoreTestOutput.txt
+java -cp "../out/production/P7.10-VideoPokerSimulator/" StraightScoreTest > straightScoreTestOutput.txt
 
-java -cp "../out/production/P7.10-VideoPokerSimulator/" FlushScoreTest > tests/flushScoreTestOutput.txt
+java -cp "../out/production/P7.10-VideoPokerSimulator/" FlushScoreTest > flushScoreTestOutput.txt
 
-java -cp "../out/production/P7.10-VideoPokerSimulator/" RoyalFlushScoreTest > tests/royalFlushScoreTestOutput.txt
+java -cp "../out/production/P7.10-VideoPokerSimulator/" RoyalFlushScoreTest > royalFlushScoreTestOutput.txt
 
-java -cp "../out/production/P7.10-VideoPokerSimulator/" CardCountScoreTest > tests/CardCountScoreTestOutput.txt
+java -cp "../out/production/P7.10-VideoPokerSimulator/" CardCountScoreTest > CardCountScoreTestOutput.txt
