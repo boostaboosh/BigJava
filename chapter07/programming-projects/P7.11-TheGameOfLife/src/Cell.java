fd1486ac2f3f4e23b211b52888b3aeae0db38918
boostaboosh <mail.laurencesnow@gmail.com>
2024-03-15 13:09:11 +0000

    Add public interfaces to game of life ch07 proj11

---
+++ chapter07/programming-projects/P7.11-TheGameOfLife/src/Cell.java
@@ -0,0 +1,32 @@
+/**
+ * The Cell class models a cell in a game of life board.
+ */
+public class Cell
+{
+   // instance variables
+   public enum State { ALIVE, DEAD }
+
+   // constructors
+   public Cell(State state)
+   {
+      // TODO: fill implementation
+   }
+
+   // methods
+   /**
+    * Set the state of this cell.
+    */
+   public void setStatus(State state)
+   {
+      // TODO: fill implementation
+   }
+
+   /**
+    * Get the state of this cell.
+    */
+   public State getStatus()
+   {
+      // TODO: fill implementation
+      return State.ALIVE;
+   }
+}
