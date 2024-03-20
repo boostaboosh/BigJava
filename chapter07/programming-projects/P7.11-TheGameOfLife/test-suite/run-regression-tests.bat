93bf9416a637ee01addc4623a81799e61e401002
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-03-20 19:59:58 +0000

    Implement hasAliveEdgeCells method & test

---
+++ chapter07/programming-projects/P7.11-TheGameOfLife/test-suite/run-regression-tests.bat
@@ -0,0 +1,16 @@
+# To be run from inside the run-regression-tests.bat directory
+# in order to prevent compiled java class files being generated
+# in new and unintended directories outside 
+# of the project directory.
+
+# Compile all java files in the src directory 
+# and place compiled java class files in 
+# the "../out/production/P7.11-TheGameOfLife/" directory.
+# And ensure the compiled class files are compatible with JDK 11, 
+# both in terms of language features and available APIs.
+javac --release 11 -d "../out/production/P7.11-TheGameOfLife/" ../src/*.java
+
+# After compilation run tests using input and output files
+# by running the class files for each test.
+java -cp "../out/production/P7.11-TheGameOfLife/" HasAliveEdgeCellsTester < hasAliveEdgeCellsTestInput.txt > hasAliveEdgeCellsTestOutput.txt
+java -cp "../out/production/P7.11-TheGameOfLife/" GetNextGenerationTester < getNextGenerationTestInput.txt > getNextGenerationTestOutput.txt
