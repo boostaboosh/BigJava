3b83b64d7f96efe14c7c96e0b0e0dcd0c3ef0594
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-03-23 23:23:01 +0000

    Add ch07 proj12 PetShopInvoiceDiscount pub interface

---
+++ chapter07/programming-projects/P7.12-PetShopInvoiceDiscount/src/Item.java
@@ -0,0 +1,50 @@
+/**
+ * An item models an entry in a pet shop invoice.
+ */
+public class Item
+{
+   private int price;
+   private boolean isPet;
+   private int quantity;
+   
+   /**
+    * Constructs a pet shop invoice item.
+    * @param price the price of one item
+    * @param isPet true if item is a pet
+    * @param quantity the amount of the item in the entry
+    */
+   public Item(double price, boolean isPet, int quantity)
+   {
+      // TODO: fill implementation
+   }
+
+   /**
+    * Gets the price of the item
+    * @return the price of the item
+    */
+   public int getPrice()
+   {
+      // TODO: fill implementation
+      return 0;
+   }
+
+   /**
+    * Gets the quantity of the item in the entry.
+    * @return the quantity of the item in the entry
+    */
+   public int getQuantity()
+   {
+      // TODO: fill implementation
+      return 0;
+   }
+
+   /**
+    * Checks if the item is a pet.
+    * @return true if the item is a pet
+    */
+   public boolean isPet()
+   {
+      // TODO: fill implementation
+      return true;
+   }
+}
