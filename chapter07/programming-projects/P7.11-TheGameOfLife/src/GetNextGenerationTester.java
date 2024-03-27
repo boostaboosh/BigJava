eb5da397943eacaaa791f88b25701b6547441558
boostaboosh <mail.laurencesnow@gmail.com>
2024-03-27 02:03:54 +0000

    Fix tests input redirection from txt files
    This commit fixes the errors with input redirection
    from text files when running tests on the game of
    life player with sample input. This way tests run,
    and now the expected result of a test is formatted
    properly and present in the output text file where
    output is redirected.
    
    bugs:
     - thanks to being able to run tests properly it
       was discovered that some of the tests to not
       pass because there is an error with the
       creation of board generations sometimes.

--- chapter07/programming-projects/P7.11-TheGameOfLife/src/GetNextGenerationTester.java
+++ chapter07/programming-projects/P7.11-TheGameOfLife/src/GetNextGenerationTester.java
@@ -8,10 +8,27 @@ public class GetNextGenerationTester
 
       System.out.println("Expected: ");
       Scanner scanner = new Scanner(System.in);
-      String expected = scanner.nextLine();
+      String expectedOutput = scanner.nextLine();
+      int index = 0;
+      while (index < expectedOutput.length())
+      {
+         char character = expectedOutput.charAt(index);
+         if (character == '\\' 
+               && expectedOutput.charAt(index + 1) == 'n' 
+               && index + 1 < expectedOutput.length())
+         {
+            System.out.println();
+            index = index + 1;
+         }
+         else
+         {
+            System.out.print(character);
+         }
+         index = index + 1;
+      }
 
       System.out.println("Result: ");
-      GameOfLife gameOfLife = new GameOfLife();
+      GameOfLife gameOfLife = new GameOfLife(scanner);
       gameOfLife.playGame();
    }
 }
