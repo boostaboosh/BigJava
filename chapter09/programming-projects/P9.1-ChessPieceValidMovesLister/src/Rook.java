1e02585578d8d23aa68402b8e4f52398d76dafea
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-06-14 19:06:53 +0200

    Add classes to ch09 prog proj01 ChessPieceValidMovesLister

---
+++ chapter09/programming-projects/P9.1-ChessPieceValidMovesLister/src/Rook.java
@@ -0,0 +1,13 @@
+/**
+ * A rook is a chess piece which can move and take forwards and backwards
+ * along files or left and right along ranks of the chess board by any
+ * number of squares as long as other pieces aren't along its trajectory.
+ */
+public class Rook
+{
+   /*
+   todo
+    fill implementation
+    be a subclass of ChessPiece
+    */
+}
