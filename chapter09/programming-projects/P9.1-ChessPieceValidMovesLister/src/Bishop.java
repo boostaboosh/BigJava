266139062cc5b9b8acd35e30ba64e7dce2dd23b2
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-06-14 19:06:53 +0200

    Add classes to ch09 prog proj01 ChessPieceValidMovesLister

---
+++ chapter09/programming-projects/P9.1-ChessPieceValidMovesLister/src/Bishop.java
@@ -0,0 +1,13 @@
+/**
+ * A bishop is a chess piece which can move and take diagonally
+ * by any number of squares as long as pieces aren't on its
+ * trajectory.
+ */
+public class Bishop
+{
+   /*
+   todo
+    fill implementation
+    be a subclass of ChessPiece
+    */
+}
---
+++ chapter09/programming-projects/P9.1-ChessPieceValidMovesLister/src/ChessPiece.java
@@ -0,0 +1,17 @@
+/**
+ * A chess piece has a position on a 8x8 chess board,
+ * and squares it can move to based on its movement pattern.
+ */
+public class ChessPiece
+{
+   /* todo:
+       fill implementation
+       *
+       Implement a method setPosition(String coordinates).
+       The coordinate string identifies the row and column in chess notation,
+       such as "d8" for the initial position of the black queen.
+       *
+       Also provide a method ArrayList<String> canMoveTo()
+       that enumerates the valid moves from the current position.
+    */
+}
