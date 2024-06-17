dbb587475ed008f458360539b86c9d8755e4722f
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-06-17 13:45:54 +0200

    Add complete ch09 prog proj01 ChessPieceValidMovesLister
    This commit adds the Pawn, Rook, Queen, and
    PlayerColour classes, and the Colour enum type.
    This commit also adds JUnit unit tests for each
    of the pieces.

---
+++ chapter09/programming-projects/P9.1-ChessPieceValidMovesLister/src/Colour.java
@@ -0,0 +1,1 @@
+public enum Colour { WHITE, BLACK };
--- chapter09/programming-projects/P9.1-ChessPieceValidMovesLister/src/Rook.java
+++ chapter09/programming-projects/P9.1-ChessPieceValidMovesLister/src/Rook.java
@@ -5,17 +5,55 @@ import java.util.ArrayList;
  * along files or left and right along ranks of the chess board by any
  * number of squares as long as other pieces aren't along its trajectory.
  */
-public class Rook
+public class Rook extends ChessPiece
 {
-   /*
-   todo
-    fill implementation
-    be a subclass of ChessPiece
+   /**
+    * See {@link ChessPiece#ChessPiece()}.
     */
+   public Rook()
+   {
+      super();
+   }
+
+   /**
+    * See {@link ChessPiece#ChessPiece(String coordinates)}.
+    * @param coordinates
+    */
+   public Rook(String coordinates)
+   {
+      super(coordinates);
+   }
 
    public ArrayList<String> canMoveTo()
    {
-      // todo: fill implementation
-      return null;
+      ArrayList<String> canMoveTo = new ArrayList<>();
+      if (super.getSquare().hasValidCoordinates())
+      {
+         ArrayList<Square> moves = new ArrayList<>();
+
+         int currentRank = super.getSquare().getRank();
+         char currentFile = super.getSquare().getFile();
+
+         for (int rank = Square.MAX_RANK; rank >= Square.MIN_RANK; rank--)
+         {
+            if (rank != currentRank)
+            {
+               moves.add(new Square(currentFile, rank));
+            }
+         }
+         for (char file = Square.MIN_FILE; file <= Square.MAX_FILE; file++)
+         {
+            if (file != currentFile)
+            {
+                moves.add(new Square(file, currentRank));
+            }
+         }
+
+         for (Square square : moves)
+         {
+            canMoveTo.add(square.getChessNotation());
+         }
+      }
+      return canMoveTo;
    }
 }
