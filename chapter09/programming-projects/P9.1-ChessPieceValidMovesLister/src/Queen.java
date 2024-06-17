afc61ee8a8a0b926976cdaf70d6e9a34c85e3703
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2024-06-17 13:45:54 +0200

    Add complete ch09 prog proj01 ChessPieceValidMovesLister
    This commit adds the Pawn, Rook, Queen, and
    PlayerColour classes, and the Colour enum type.
    This commit also adds JUnit unit tests for each
    of the pieces.

--- chapter09/programming-projects/P9.1-ChessPieceValidMovesLister/src/Queen.java
+++ chapter09/programming-projects/P9.1-ChessPieceValidMovesLister/src/Queen.java
@@ -9,17 +9,99 @@ import java.util.ArrayList;
  * or along ranks and files, as long as other pieces aren't on its
  * movement trajectory.
  */
-public class Queen
+public class Queen extends ChessPiece
 {
-   /*
-   todo
-    fill implementation
-    be a subclass of ChessPiece
+   /**
+    * See {@link ChessPiece#ChessPiece()}.
     */
+   public Queen()
+   {
+      super();
+   }
+
+   /**
+    * See {@link ChessPiece#ChessPiece(String coordinates)}.
+    * @return
+    */
+   public Queen(String coordinates)
+   {
+      super(coordinates);
+   }
 
    public ArrayList<String> canMoveTo()
    {
-      // todo: fill implementation
-      return null;
+      ArrayList<String> canMoveTo = new ArrayList<>();
+      if (super.getSquare().hasValidCoordinates())
+      {
+         ArrayList<Square> moves = new ArrayList<>();
+
+         final char CURRENT_FILE = super.getSquare().getFile();
+         final int CURRENT_RANK = super.getSquare().getRank();
+
+         for (int rank = Square.MIN_RANK; rank <= Square.MAX_RANK; rank++)
+         {
+            if (rank != CURRENT_RANK)
+            {
+               moves.add(new Square(CURRENT_FILE, rank));
+            }
+         }
+
+         for (char file = Square.MIN_FILE; file <= Square.MAX_FILE; file++)
+         {
+            if (file != CURRENT_FILE)
+            {
+               moves.add(new Square(file, CURRENT_RANK));
+            }
+         }
+
+         final int DISTANCE_FROM_TOP_RANK = Square.MAX_RANK - CURRENT_RANK;
+         final int DISTANCE_FROM_LEFT_FILE = CURRENT_FILE - Square.MIN_FILE;
+         final int DISTANCE_FROM_TOP_AND_LEFT_EDGES = Math.min
+               (DISTANCE_FROM_TOP_RANK, DISTANCE_FROM_LEFT_FILE);
+         final Square TOP_LEFT_TO_BOTTOM_RIGHT_DIAGONAL_START_SQUARE = new Square
+               (
+                     (char) (CURRENT_FILE - DISTANCE_FROM_TOP_AND_LEFT_EDGES),
+                     CURRENT_RANK + DISTANCE_FROM_TOP_AND_LEFT_EDGES
+               );
+
+         char file = TOP_LEFT_TO_BOTTOM_RIGHT_DIAGONAL_START_SQUARE.getFile();
+         int rank = TOP_LEFT_TO_BOTTOM_RIGHT_DIAGONAL_START_SQUARE.getRank();
+         while (file <= Square.MAX_FILE && rank >= Square.MIN_RANK)
+         {
+            if (file != CURRENT_FILE && rank != CURRENT_RANK)
+            {
+               moves.add(new Square(file, rank));
+            }
+            file = (char) (file + 1);
+            rank = rank - 1;
+         }
+
+         final int DISTANCE_FROM_BOTTOM_RANK = CURRENT_RANK - Square.MIN_RANK;
+         final int DISTANCE_FROM_BOTTOM_AND_LEFT_EDGES =
+               Math.min(DISTANCE_FROM_BOTTOM_RANK, DISTANCE_FROM_LEFT_FILE);
+         final Square BOTTOM_LEFT_TO_TOP_RIGHT_DIAGONAL_START_SQUARE = new Square
+            (
+                  (char) (CURRENT_FILE - DISTANCE_FROM_BOTTOM_AND_LEFT_EDGES),
+                  CURRENT_RANK - DISTANCE_FROM_BOTTOM_AND_LEFT_EDGES
+            );
+
+         file = BOTTOM_LEFT_TO_TOP_RIGHT_DIAGONAL_START_SQUARE.getFile();
+         rank = BOTTOM_LEFT_TO_TOP_RIGHT_DIAGONAL_START_SQUARE.getRank();
+         while (file <= Square.MAX_FILE && rank <= Square.MAX_RANK)
+         {
+            if (file != CURRENT_FILE && rank != CURRENT_RANK)
+            {
+               moves.add(new Square(file, rank));
+            }
+            file = (char) (file + 1);
+            rank = rank + 1;
+         }
+
+         for (Square square : moves)
+         {
+            canMoveTo.add(square.getChessNotation());
+         }
+      }
+      return canMoveTo;
    }
 }
