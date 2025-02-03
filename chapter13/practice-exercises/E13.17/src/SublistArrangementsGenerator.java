a5367e35845c299bb819d0381f12fe9007c47225
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2025-02-03 08:52:43 +0000

    Add problem solving for E13.17

---
+++ chapter13/practice-exercises/E13.17/src/SublistArrangementsGenerator.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class SublistArrangementsGenerator
+{
+   /**
+    * Generates and returns an array list containing all possible arrangements 
+    * of the sequence of elements from the input list into lists of sublists.
+    * <p> 
+    * For example, the input: (2,8,4)
+    * returns the output:
+    * ((2,8,4), ((2),(8),(4)), ((2,8),(4)), ((2),(8,4))).
+    * @param arrayList the input list
+    * @return an array list containing all possible arrangements 
+    * of the sequence of elements from the input list into lists of sublists.
+    * @param <T> the type of the elements in the array list
+    */
+   public static <T> ArrayList<ArrayList<T>> getSublistArrangements(ArrayList<T> arrayList)
+   {
+      // todo
+      return null;
+   }
+}
