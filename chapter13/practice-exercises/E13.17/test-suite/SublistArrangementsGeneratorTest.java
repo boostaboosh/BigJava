ecc79a15442764033632893adc9a48a6b2d3b2b1
boostaboosh <mail.laurencesnow@gmail.com>
2025-02-09 00:49:29 +0000

    Add test for E13.17

--- chapter13/practice-exercises/E13.17/test-suite/SublistArrangementsGeneratorTest.java
+++ chapter13/practice-exercises/E13.17/test-suite/SublistArrangementsGeneratorTest.java
@@ -1,7 +1,37 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SublistArrangementsGeneratorTest
 {
-   // todo: write test for recursive method
+   @Test
+   public void generateSublistArrangementsOf3ElementListTest() {
+      ArrayList<Integer> threeElementsList = new ArrayList<Integer>();
+      threeElementsList.add(2);
+      threeElementsList.add(8);
+      threeElementsList.add(4);
+      
+      ArrayList<ArrayList<ArrayList<Integer>>> expected = new ArrayList<>();
+      expected.add(new ArrayList<>(List.of(new ArrayList<>(List.of(2, 8, 4))))); // Full list
+      expected.add(new ArrayList<>(List.of( // Single elements grouped
+            new ArrayList<>(List.of(2)),
+            new ArrayList<>(List.of(8)),
+            new ArrayList<>(List.of(4))
+      )));
+      expected.add(new ArrayList<>(List.of( // Subset (2,8) with (4)
+            new ArrayList<>(List.of(2, 8)),
+            new ArrayList<>(List.of(4))
+      )));
+      expected.add(new ArrayList<>(List.of( // Subset (2) with (8,4)
+            new ArrayList<>(List.of(2)),
+            new ArrayList<>(List.of(8, 4))
+      )));
+      ;
+      ArrayList<ArrayList<ArrayList<Integer>>> actual = SublistArrangementsGenerator.getSublistArrangements(threeElementsList);
 
+      assertEquals("Mismatch in number of generated sublists", expected.size(), actual.size());
+      assertTrue("Actual result does not contain all expected elements", actual.containsAll(expected));
+   }
 }
