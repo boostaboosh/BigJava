a4d11ab27d2b43479b01241a60fc651a0f541343
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2025-05-24 22:34:13 +0100

    Add E20.1

---
+++ chapter20/practice-exercises/E20.1-2/src/ColorFrameViewer.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+/**
+ * Color frame viewer
+ */
+public class ColorFrameViewer
+{
+    public static void main(String[] args)
+    {
+        ColorFrame colorFrame = new ColorFrame();
+    }
+}
